4f3d6bf2a542116eb506862256ec2c42bde7b2a2
Lucie Brackenier <lucie.brackenier@ynov.com>
2022-05-03 08:54:45 +0200

    implemented admin middleware and protected concerned routes with it

--- routes/user-route.js
+++ routes/user-route.js
@@ -3,11 +3,15 @@ const router = express.Router();
 
 const userController = require('../controller/user-controller');
 
+const authentification = require('../middleware/authenticate-middleware');
+const isUserAdmin = require('../middleware/admin-middleware');
+
+
 router
 .get('/', userController.get_all_users)
 .post('/register', userController.create_user)
-.patch('/:id', userController.modify_user)
-.delete('/:id', userController.delete_user)
+.patch('/:id', [authentification, isUserAdmin],userController.modify_user)
+.delete('/:id', [authentification, isUserAdmin], userController.delete_user)
 .post('/login', userController.user_login);
 
 module.exports = router;
